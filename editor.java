e4e7034d98a5dccdc1532a89b2ab676108ead276
rafobitro <rafobitro@gmail.com>
2025-08-27 11:12:23 -0700

    testing manual git via terminal

---
+++ editor.java
@@ -0,0 +1,11 @@
+public class editor{
+
+    public static void editor_loop (){
+        
+    }
+    public static void main (String args[])
+    {
+       //open editor using args file
+       //exit 
+    }
+}
