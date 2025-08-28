fa42765a610df664daea9eb69971c3741fd8a0d6
rafobitro <rafobitro@gmail.com>
2025-08-27 20:41:28 -0700

    first commit

--- editor.java
+++ editor.java
@@ -1,11 +1,43 @@
-public class editor{
+import java.util.Scanner;
 
+public class editor{
+/*
+    //declar array of links to strings
+    //point cordinate x-line y-word y2-character init to 0 0 0
     public static void editor_loop (){
-        
+        while(true)
+        {
+            //print
+            //wait response
+            //if esceape brake
+            //if charcter add
+            //if movement change cordinates
+            //reprint
+        }
     }
+
+    public static void init(string file_name)
+    {
+        //open file
+        //copy into array of links to string
+        //close file
+    }  
+    
+    public static void print()
+    {
+        //print array of links to string
+    }  
+*/
     public static void main (String args[])
     {
-       //open editor using args file
-       //exit 
+        //test
+        
+
+
+
+        //reed args file
+        //copy into memmory
+        //editor_loop
+        //exit 
     }
 }
